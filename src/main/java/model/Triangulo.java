23685389e8ad1b04ea96bddbaf9ccf723286285b
Mash <mashjov13@outlook.com>
2022-02-10 19:49:47 -0600

    clase principal JAVA terminada

---
+++ src/main/java/model/Triangulo.java
@@ -0,0 +1,55 @@
+/*  
+    APP 06: CALCULOS EQUILATERO JSP
+    AUTORA: Maria Tchijov Cruz
+    FECHA: 10/02/22
+    Archivo JAVA para calcular base y altura de un triángulo
+
+ */
+
+package model;
+
+public class Triangulo 
+{
+    //ATRIBUTOS DE LA CLASE
+    private int base;
+    private int altura;
+    private int perimetro;
+    private int area;
+    
+    //CONSTRUCTOR QUE CONVIERTE EL TEXTO INGRESADO EN INT
+    public Triangulo(String base, String altura) 
+    {
+        this.setBase(Integer.parseInt(base));
+        this.setAltura(Integer.parseInt(altura));
+    }
+    
+    //MÉTODOS GET Y SET PARA ATRIBUTOS PRIVADOS
+    public int getBase() {return base;}
+    public void setBase(int base) {this.base = base;}
+    public int getAltura() {return altura;}
+    public void setAltura(int altura) {this.altura = altura;}
+    public int getPerimetro() {return perimetro;}
+    public void setPerimetro(int perimetro) {this.perimetro = perimetro;}
+    public int getArea() {return area;}
+    public void setArea(int area) {this.area = area;}
+    
+    //MÉTODOS PRINCIPALES DE LA CLASE
+    //perimetro : CALCULA EL PERÍMETRO
+    public void perimetro()
+    {
+        //Se calcula el perímetro con la fórmula P = 3base
+        int perimetro = 3 * base;
+        //Se guarda el resultado
+        this.setPerimetro(perimetro);
+    }
+    
+    //area : CALCULA EL ÁREA
+    public void area()
+    {
+        //Se calcula el área con la fórmula A = (base * altura) / 2
+        int area = (base * altura) / 2;
+        //Se guarda el resultado
+        this.setArea(area);
+    }
+    
+}
